f7dc97df816a1555f30b0cd524c4c9219f174136
Manikanta Sunkara <97591820+raghavendhra-sunkara@users.noreply.github.com>
2022-02-21 16:00:45 +0530

    #include <stdio.h>
    int main()
    {
       int x, y, i, gcd;
       scanf("%d %d", &x, &y);
    
       for (i = 1; i <= x && i <= y; i++)\n   {
          if (x % i == 0 && y % i == 0)
             gcd = i;
       }
    
       printf("%d",gcd);
    
       return 0;
    }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int x, y, i, gcd;
+   scanf("%d %d", &x, &y);
+
+   for (i = 1; i <= x && i <= y; i++)\n   {
+      if (x % i == 0 && y % i == 0)
+         gcd = i;
+   }
+
+   printf("%d",gcd);
+
+   return 0;
+}
