9f2d9f524640f03c1ef139a69bf65448bf60f756
Manikanta Sunkara <97591820+raghavendhra-sunkara@users.noreply.github.com>
2022-03-06 21:41:02 +0530

    #include<stdio.h> int main() { 	int n,i,j; 	scanf("%d",&n); 	for(j=1;j<=n;j++) 	{ 	    for(i=n;i>0;i--) 	    { 		    printf("%d ",i); 	    } 	    printf(" "); 	} }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n-i+1;j++)
-        {
-            printf("%d",j);
-        }
-        printf("
+	int n,i,j;
+	scanf("%d",&n);
+	for(j=1;j<=n;j++)
+	{
+	    for(i=n;i>0;i--)
+	    {
+		    printf("%d ",i);
+	    }
+	    printf("
 ");
-    }
-    return 0;
+	}
 }
