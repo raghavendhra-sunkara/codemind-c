0d681a23be9aebd2799136b484a1c2e47efda01c
Manikanta Sunkara <97591820+raghavendhra-sunkara@users.noreply.github.com>
2022-04-25 22:23:30 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,x,y,d;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0)         {             x+=arr[i];         }         else         {             y+=arr[i];         }     }     if(x>y)     {         d=x-y;     }     else     {         d=y-x;     }     if(d%4==0)     {         printf("X");     }     else     {         printf("Y");     } }

---
+++ Predict_the_winner.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,x,y,d;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            x+=arr[i];
+        }
+        else
+        {
+            y+=arr[i];
+        }
+    }
+    if(x>y)
+    {
+        d=x-y;
+    }
+    else
+    {
+        d=y-x;
+    }
+    if(d%4==0)
+    {
+        printf("X");
+    }
+    else
+    {
+        printf("Y");
+    }
+}
