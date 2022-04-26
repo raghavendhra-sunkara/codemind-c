f876f3a175e95ba7b986cd17cec3246e71791d04
Manikanta Sunkara <97591820+raghavendhra-sunkara@users.noreply.github.com>
2022-04-26 10:17:19 +0530

    #include<stdio.h> int main() {     int n,i,v,k;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d %d",&v,&k);         printf("%d ",v+k);     };     return 0; };

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,v,k;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d %d",&v,&k);
+        printf("%d
+",v+k);
+    };
+    return 0;
+};
